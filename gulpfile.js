b6fe51e847ef5980bda26c3b00847c8effc307f1
Marco Secreto <msegreto@miceover.com>
2014-07-23 21:10:32 -0700

    Continuation

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+var path = require('path');
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+//gulp.task('css', function () {
+//  gulp.src('app/**/*.css').pipe(refresh(server));
+//});
+
+gulp.task('less', function () {
+  gulp.src('./src/css/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./compiled/css'));
+});
+
+gulp.task('default', function () {
+
+  gulp.watch('./src/css/**/*.less', function (event) {
+    //gulp.run('css');
+    gulp.run('less');
+  });
+});
