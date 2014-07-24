67b36b648ed09834a03ba7a57d56d8ebaffe4620
Jeremia Kimelman <jbkimelman@gmail.com>
2014-07-24 00:14:22 -0700

    making getting set up much easier

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ localfreeweb.org
 ================
 
 Front end for the Local Free Web project
+
+## To get started with development
+After you `git clone` run `npm install` and then just `gulp`
--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,39 @@ var path = require('path');
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var livereload = require('gulp-livereload');
+var httpServer = require('http-server');
+var opener = require('opener');
+var portfinder = require('portfinder');
 
 //gulp.task('css', function () {
 //  gulp.src('app/**/*.css').pipe(refresh(server));
 //});
 
-gulp.task('less', function () {
+gulp.task('less', function (cb) {
   gulp.src('./src/css/main.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./compiled/css'));
+    cb();
 });
 
-gulp.task('default', function () {
+gulp.task('default', ['less'], function () {
+  var lr = livereload;
+  lr.listen();
 
-  gulp.watch('./src/css/**/*.less', function (event) {
-    //gulp.run('css');
-    gulp.run('less');
+  gulp.watch('./src/css/**/*.less', ['less']);
+  gulp.watch('./compiled/**').on('change', function(file) {
+    console.log('build changed', file.path);
+    lr.changed(file.path);
   });
+
+  portfinder.getPort(function (err, port) {
+    var server = httpServer.createServer({
+      root: './'
+    }).listen(port);
+    opener('http://localhost:'+port);
+  })
+
 });
